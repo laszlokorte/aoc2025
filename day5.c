7165559cf127a9b1a308cb0b7d7842aa269bff24
Laszlo Korte <email@laszlokorte.de>
2025-12-05 14:46:01 +0100

    day5

---
+++ day5.c
@@ -0,0 +1,107 @@
+#include "day.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEBUG(...) {};
+// #define DEBUG(...) printf(__VA_ARGS__);
+
+long long calc(size_t size, char *buffer, bool all_in_ranges);
+int compare_range(const void *a, const void *b);
+
+long long part1(size_t size, char *buffer) { return calc(size, buffer, false); }
+long long part2(size_t size, char *buffer) { return calc(size, buffer, true); }
+
+struct range {
+  long long start;
+  long long end;
+};
+
+long long calc(size_t size, char *buffer, bool all_in_ranges) {
+  long long fresh = 0;
+  int num_ranges = 1;
+  int pos = 0;
+  while (buffer[pos] != '\0' && pos <= (int)size &&
+         (buffer[pos] != '\n' || buffer[pos + 1] != '\n')) {
+    if (buffer[pos] == '\n') {
+      num_ranges++;
+    }
+    pos++;
+  }
+
+  struct range ranges[num_ranges];
+  int i = 0;
+  for (int r = 0; r < num_ranges; r++) {
+    struct range new_range = {0};
+    while (buffer[i] != '\0' && pos <= (int)size && (buffer[i] != '-')) {
+      int d = buffer[i] - '0';
+      new_range.start = new_range.start * 10 + d;
+      i++;
+    }
+    i++;
+    while (buffer[i] != '\0' && pos <= (int)size && (buffer[i] != '\n')) {
+      int d = buffer[i] - '0';
+      new_range.end = new_range.end * 10 + d;
+      i++;
+    }
+    i++;
+    ranges[r] = new_range;
+  }
+
+  if (!all_in_ranges) {
+    for (int r = 0; r < num_ranges; r++) {
+      DEBUG("%llu-%llu\n", ranges[r].start, ranges[r].end);
+    }
+    while (buffer[i] != '\0' && pos <= (int)size) {
+      long long acc = 0;
+      while (buffer[i] != '\0' && i <= (int)size && buffer[i] != '\n') {
+        int d = buffer[i] - '0';
+        acc = acc * 10 + d;
+        i++;
+      }
+
+      i++;
+      DEBUG("%llu\n", acc);
+      for (int r = 0; r < num_ranges; r++) {
+        struct range range = ranges[r];
+        if (range.start <= acc && acc <= range.end) {
+          DEBUG("fresh: %llu\n", acc);
+          fresh++;
+          break;
+        }
+      }
+    }
+  } else {
+    qsort(ranges, num_ranges, sizeof(struct range), compare_range);
+    long long cursor = 0;
+    for (int r = 0; r < num_ranges; r++) {
+      struct range rn = ranges[r];
+      if (cursor < rn.start) {
+        DEBUG("overlap: cursor: %lli,  end: %lli, grow: %lli\n", cursor, rn.end,
+              rn.end - rn.start + 1);
+        fresh += rn.end - rn.start + 1;
+        cursor = rn.end;
+
+      } else if (cursor >= rn.start && cursor < rn.end) {
+        DEBUG("overlap: cursor: %lli,  end: %lli, grow: %lli\n", cursor, rn.end,
+              rn.end - cursor);
+        fresh += rn.end - cursor;
+        cursor = rn.end;
+      }
+    }
+  }
+
+  return fresh;
+}
+
+int compare_range(const void *a, const void *b) {
+  struct range r1 = *(struct range *)a;
+  struct range r2 = *(struct range *)b;
+  if (r1.start < r2.start)
+    return -1;
+  if (r1.start == r2.start && r1.end < r2.end)
+    return -1;
+  if (r1.start == r2.start && r1.end == r2.end)
+    return 0;
+  return 1;
+}
