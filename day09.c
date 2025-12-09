24a4673c72503c6099555831b78908f64d7547c9
Laszlo Korte <email@laszlokorte.de>
2025-12-10 00:50:08 +0100

    add comments

--- day09.c
+++ day09.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+#define DIMS 2
+
 #define DEBUG(...) {};
 // #define DEBUG(...) printf(__VA_ARGS__);
 
@@ -10,54 +12,63 @@
 
 long long calc(size_t size, char *buffer, bool convex);
 
-long long part1(size_t size, char *buffer) { return calc(size, buffer, true); }
-long long part2(size_t size, char *buffer) { return calc(size, buffer, false); }
+long long part1(size_t size, char *buffer) { return calc(size, buffer, false); }
+long long part2(size_t size, char *buffer) { return calc(size, buffer, true); }
 
 long long calc(size_t size, char *buffer, bool convex) {
-  long long largest = 0;
+  long long largest_area = 0;
 
   size_t line_count = 1;
-  int pos = 0;
-  while (buffer[pos] != '\0' && pos <= (int)size &&
-         (buffer[pos] != '\n' || buffer[pos + 1] != '\n')) {
-    if (buffer[pos] == '\n') {
-      line_count++;
+  {
+    // count total lines
+    int pos = 0;
+    while (buffer[pos] != '\0' && pos <= (int)size) {
+      if (buffer[pos] == '\n') {
+        line_count++;
+      }
+      pos++;
     }
-    pos++;
   }
-  long long corners[line_count][2] = {};
-  int c = 0;
-  for (size_t l = 0; l < line_count; l++) {
-    for (size_t n = 0; n < 2; n++) {
-      long long acc = 0;
-      while (buffer[c] != '\0' && c <= (int)size && buffer[c] != '\n' &&
-             buffer[c] != ',') {
-        int d = buffer[c] - '0';
-        acc = acc * 10 + d;
+  //
+  long long vertices[line_count][DIMS] = {};
+  {
+    int c = 0;
+    for (size_t l = 0; l < line_count; l++) {
+      for (size_t n = 0; n < DIMS; n++) {
+        long long acc = 0;
+        while (buffer[c] != '\0' && c <= (int)size && buffer[c] != '\n' &&
+               buffer[c] != ',') {
+          int d = buffer[c] - '0';
+          acc = acc * 10 + d;
+          c++;
+        }
+        vertices[l][n] = acc;
         c++;
       }
-      corners[l][n] = acc;
-      c++;
     }
   }
   for (size_t a = 0; a < line_count; a++) {
     for (size_t b = 0; b < line_count; b++) {
-      long long minx = MIN(corners[a][0], corners[b][0]);
-      long long miny = MIN(corners[a][1], corners[b][1]);
-      long long maxx = MAX(corners[a][0], corners[b][0]);
-      long long maxy = MAX(corners[a][1], corners[b][1]);
+      long long minx = MIN(vertices[a][0], vertices[b][0]);
+      long long miny = MIN(vertices[a][1], vertices[b][1]);
+      long long maxx = MAX(vertices[a][0], vertices[b][0]);
+      long long maxy = MAX(vertices[a][1], vertices[b][1]);
       long long w = maxx - minx;
       long long h = maxy - miny;
       long long area = (w + 1) * (h + 1);
-      if (area > largest) {
-        if (!convex) {
+
+      if (area > largest_area) {
+        // for part 2 check convexity
+        if (convex) {
           bool overlap = false;
-          for (size_t c = 0; c < line_count; c++) {
+          for (size_t c = 0; c < line_count && !overlap; c++) {
             size_t cc = (c + 1) % line_count;
-            long long x1 = MIN(corners[c][0], corners[cc][0]);
-            long long y1 = MIN(corners[c][1], corners[cc][1]);
-            long long x2 = MAX(corners[c][0], corners[cc][0]);
-            long long y2 = MAX(corners[c][1], corners[cc][1]);
+
+            long long x1 = MIN(vertices[c][0], vertices[cc][0]);
+            long long y1 = MIN(vertices[c][1], vertices[cc][1]);
+            long long x2 = MAX(vertices[c][0], vertices[cc][0]);
+            long long y2 = MAX(vertices[c][1], vertices[cc][1]);
+
             overlap = overlap ||
                       !(x2 <= minx || maxx <= x1 || y2 <= miny || maxy <= y1);
           }
@@ -65,9 +76,10 @@ long long calc(size_t size, char *buffer, bool convex) {
             continue;
           }
         }
-        largest = area;
+        largest_area = area;
       }
     }
   }
-  return largest;
+
+  return largest_area;
 }
