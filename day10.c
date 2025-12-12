b5b6737511f33c0871b596bc2000cf6e91ddce00
Laszlo Korte <email@laszlokorte.de>
2025-12-12 03:06:15 +0100

    cleanup day10

--- day10.c
+++ day10.c
@@ -1,11 +1,14 @@
 #include "day.h"
+#include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-#define TMP_FILE_NAME "tmp_problem.smt2"
+#define MAX_BUTTONS 32
+#define MAX_DIMS 16
+
 #define DEBUG(...) {};
 // #define DEBUG(...) printf(__VA_ARGS__);
 
@@ -127,12 +130,10 @@ long long part2(size_t size, char *buffer) {
   }
   size_t p = 0;
   for (size_t l = 0; l < line_count; l++) {
-    // DEBUG("xxx\n");
-    // bitset of turnd on lights
-    unsigned int goal_light[16] = {};
+    unsigned int goal_light[MAX_DIMS] = {};
     {
       size_t line_start = p;
-      // parse string between [ and ] into bitset
+      // parse string between { and } into bitset
       while (buffer[p] != '{') {
         p++;
       }
@@ -155,11 +156,10 @@ long long part2(size_t size, char *buffer) {
       p++;
       p++;
     }
-    size_t buttons = 0;
     size_t button_start = p;
-    { // count number of buttons (the number of parenthesis)
+    {
+      // count number of buttons (the number of parenthesis)
       while (buffer[p] == '(' && p <= size) {
-        buttons++;
         while (buffer[p] != ')' && p <= size) {
           p++;
         }
@@ -171,91 +171,128 @@ long long part2(size_t size, char *buffer) {
       }
       p++;
     }
-    size_t pp = button_start; // reset cursor the position where the first "("
-                              // in the line is
+
+    unsigned short toggle_mask[MAX_BUTTONS][MAX_DIMS] = {};
     unsigned int button_count = 0;
-    // parse buttons
-    unsigned short toggle_mask[32][16] = {};
-    while (buffer[pp] == '(' && pp <= size) {
-      pp++;
-      // parse each element of the button
-      while (buffer[pp] != ' ' && pp <= size) {
-        size_t accum = 0;
-        while (buffer[pp] != ')' && buffer[pp] != ',' && pp <= size) {
-          accum = accum * 10 + (buffer[pp] - '0');
+    {
+      // reset cursor the position where the first "("
+      // in the line is
+      size_t pp = button_start;
+      // parse buttons
+      while (buffer[pp] == '(' && pp <= size) {
+        pp++;
+        // parse each element of the button
+        while (buffer[pp] != ' ' && pp <= size) {
+          size_t accum = 0;
+          while (buffer[pp] != ')' && buffer[pp] != ',' && pp <= size) {
+            accum = accum * 10 + (buffer[pp] - '0');
+            pp++;
+          }
+          // turn the button labels vector
+          toggle_mask[button_count][accum] = 1;
+
           pp++;
         }
-        // turn the button labels into a bitset
-        toggle_mask[button_count][accum] = 1;
+        button_count++;
 
         pp++;
       }
-      button_count++;
-
-      pp++;
     }
 
-    // use z3 solver
-    // create file to write the problem into
-    FILE *tmp = fopen(TMP_FILE_NAME, "w");
+    // use external z3 solver via subprocess
+    //
+    // setup pipes to write/read to z3 process
+    // parent writes -> child reads
+    int in_pipe[2];
+    // child writes -> parent reads
+    int out_pipe[2];
+    // in_pipe[0] is for reading data thats writtes to in_pipe[1]
+    pipe(in_pipe);
+    // in_pipe[0] is for reading data thats writtes to in_pipe[1]
+    pipe(out_pipe);
 
-    if (!tmp) {
+    // fork to launch z3
+    pid_t child_pid = fork();
+    if (child_pid == 0) {
+      // child
+      dup2(in_pipe[0], STDIN_FILENO);   // redirect in_pipe to stdin of z3
+      dup2(out_pipe[1], STDOUT_FILENO); // redirect stdout of z3 to out_pipe
+      close(out_pipe[0]); // we do not want to read from the out_pipe
+      close(in_pipe[1]);  // we do not want to write to the in_pipe
+      // start z3
+      execlp("z3", "z3", "-in", "-smt2", NULL);
+      // exit if done
+      _exit(1);
+    }
+
+    // we do not want to read from in_pipe, thats for z3 to read from
+    close(in_pipe[0]);
+    // we do not want to write into out_pipe, thats for z3 to write into
+    close(out_pipe[1]);
+
+    // open in_pipe for writing input into z3
+    FILE *to_z3 = fdopen(in_pipe[1], "w");
+    // open out_pipe for reading from z3 output
+    FILE *from_z3 = fdopen(out_pipe[0], "r");
+
+    if (!to_z3 || !from_z3) {
+      fprintf(stderr, "Error fdopen\n");
       return 1;
     }
-    int min = 0;
     {
       // define a variable for each button
       for (size_t bi = 0; bi < button_count; bi++) {
-        fprintf(tmp, "(declare-const x%lu Int)\n", bi);
+        fprintf(to_z3, "(declare-const x%lu Int)\n", bi);
       }
 
-      fprintf(tmp, "\n");
+      fprintf(to_z3, "\n");
     }
     {
 
       // require number of button presses to be positive
       for (size_t bi = 0; bi < button_count; bi++) {
-        fprintf(tmp, "(assert (>= x%lu 0))\n", bi);
+        fprintf(to_z3, "(assert (>= x%lu 0))\n", bi);
       }
 
-      fprintf(tmp, "\n");
+      fprintf(to_z3, "\n");
     }
 
     // for each target joltage require the number
     // of relevant button presses to sum to the exact value
-    for (size_t li = 0; li < 16; li++) {
-      fprintf(tmp, "(assert (= (+");
+    for (size_t li = 0; li < MAX_DIMS; li++) {
+      fprintf(to_z3, "(assert (= (+");
       for (size_t bi = 0; bi < button_count; bi++) {
-        fprintf(tmp, " (* x%lu %d)", bi, toggle_mask[bi][li]);
+        fprintf(to_z3, " (* x%lu %d)", bi, toggle_mask[bi][li]);
       }
 
-      fprintf(tmp, ") %d))", goal_light[li]);
-      fprintf(tmp, "\n");
+      fprintf(to_z3, ") %d))", goal_light[li]);
+      fprintf(to_z3, "\n");
     }
+
+    // define the goal
     {
       // minimize the total number of button presses
-      fprintf(tmp, "(minimize (+");
+      fprintf(to_z3, "(minimize (+");
       for (size_t bi = 0; bi < button_count; bi++) {
-        fprintf(tmp, " x%lu", bi);
+        fprintf(to_z3, " x%lu", bi);
       }
 
-      fprintf(tmp, "))\n");
+      fprintf(to_z3, "))\n");
     }
     // run the solver
-    fprintf(tmp, "(check-sat)\n");
+    fprintf(to_z3, "(check-sat)\n");
     // output the result
-    fprintf(tmp, "(get-objectives)\n");
-    fclose(tmp);
-    // feed the problem description to z3
-    FILE *fp = popen("z3 -T:0 tmp_problem.smt2", "r");
-    if (!fp) {
-      fprintf(stderr, "z3 could not be started");
-      return 1;
-    }
+    fprintf(to_z3, "(get-objectives)\n");
+    // make z3 exit when done
+    fprintf(to_z3, "(exit)\n");
+    fflush(to_z3);
+
     char buffer[1024];
     void *read = 0;
     // read z3 output
-    while ((read = fgets(buffer, sizeof(buffer), fp)) != NULL) {
+    //
+    int min = 0;
+    while ((read = fgets(buffer, sizeof(buffer), from_z3)) != NULL) {
       // look for lines that contain both opening and closing paren
       char *start = strchr(buffer, '(');
       char *end = strrchr(buffer, ')');
@@ -271,11 +308,17 @@ long long part2(size_t size, char *buffer) {
             last_space++;
           }
           min = accum;
+          // we already have the result, force kill fork if not gone yet.
+          kill(child_pid, SIGKILL);
+          waitpid(child_pid, NULL, 0);
+          break;
         }
       }
     }
-    pclose(fp);
-    unlink(TMP_FILE_NAME);
+
+    // close file descriptors
+    fclose(from_z3);
+    fclose(to_z3);
     sum += min;
   }
 
